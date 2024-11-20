8dc64ba9ffb7724edd920dbafbd304259687a43a
Robert Snedegar <viking@gmail.com>
2024-11-20 10:10:16 -0800

    Fix up some of the unicode markers to better fit the rendered text. Add marker symbols to highlight winning positions.

--- mnkgame/markers.go
+++ mnkgame/markers.go
@@ -4,11 +4,19 @@ package mnkgame
 type Marker string
 
 const (
-	filledBlackCircle = "⬤" // U+2B24 BLACK LARGE CIRCLE
-	//filledBlackCircle = "⚫" // U+26AB MEDIUM BLACK CIRCLE
+	filledBlackCircle = "⚫" // U+26AB MEDIUM BLACK CIRCLE
 	filledWhiteCircle = "⭘" // U+2B58 HEAVY CIRCLE
 	blackX            = "🗙" // U+1F5D9 CANCELLATION X
 
+	winMarkerUpArrowWhite    = "▵" // U+25B5 - WHITE UP-POINTING SMALL TRIANGLE
+	winMarkerDownArrowWhite  = "▿" // U+25BF - WHITE DOWN-POINTING SMALL TRIANGLE
+	winMarkerRightArrowWhite = "▹" // U+25B9 - WHITE RIGHT-POINTING SMALL TRIANGLE
+	winMarkerLeftArrowWhite  = "◃" // U+25C3 - WHITE LEFT-POINTING SMALL TRIANGLE
+
+	winMarkerUpArrowBlack    = "▴" // U+25B4 - BLACK UP-POINTING SMALL TRIANGLE
+	winMarkerDownArrowBlack  = "▾" // U+25BE - BLACK DOWN-POINTING SMALL TRIANGLE
+	winMarkerRightArrowBlack = "▸" // U+25B8 - BLACK RIGHT-POINTING SMALL TRIANGLE
+	winMarkerLeftArrowBlack  = "◂" // U+25C2 - BLACK LEFT-POINTING SMALL TRIANGLE
 )
 
 // Predefine some markers.
